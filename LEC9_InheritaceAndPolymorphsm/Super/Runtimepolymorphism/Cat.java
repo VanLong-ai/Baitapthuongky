45384bd0cbdc5b87b5edcc91d95b74f9d0047844
VanLong-ai <akim84496@gmail.com>
2026-02-05 22:12:38 +0700

    Update Cat.java

--- LEC9_InheritaceAndPolymorphsm/Super/Runtimepolymorphism/Cat.java
+++ LEC9_InheritaceAndPolymorphsm/Super/Runtimepolymorphism/Cat.java
@@ -3,6 +3,7 @@ package LEC9_InheritaceAndPolymorphsm.Super.Runtimepolymorphism;
 class Cat extends Animal {
  @Override
  void makeSound() {
- System.out.println("meo meo meo meo meo con meo ngu ngoc dang yeu cute pho mai que cua chi");
+ System.out.println("meo meo");
  }
+
 }
