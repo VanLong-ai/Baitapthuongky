01d3f7e759a497e83ca22d5479e0e99663b94bd7
VanLong-ai <akim84496@gmail.com>
2026-02-05 22:29:06 +0700

    Update Cat.java

--- LEC9_InheritaceAndPolymorphsm/Super/methodoverriding/Cat.java
+++ LEC9_InheritaceAndPolymorphsm/Super/methodoverriding/Cat.java
@@ -1,9 +1,10 @@
 
 package LEC9_InheritaceAndPolymorphsm.Super.methodoverriding;
 public class Cat  extends Animal{
-   
+   @Override
 void makeSound() {
 System.out.println("Meows meows");
 }
   
 }
+
